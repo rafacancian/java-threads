53cd40667f2249a36351965ad6218b4146a83277
rafacancian <cancian.rafa@gmail.com>
2024-10-11 21:57:17 +0100

    init project with spring boot and java

---
+++ src/test/java/com/java_threads/JavaThreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_threads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaThreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
